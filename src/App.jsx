0c1cfd493a17242ca5f7e156b5ed7a6572b9a8cb
Prabh ‪‪ <singh20prabh@gmail.com>
2025-01-30 00:51:17 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <main>
+      <NavBar />
       <Hero />
       <About />
     </main>
