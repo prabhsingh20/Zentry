dfb7084a37b5ee88c18a507de728426b2e8a173f
Prabh ‪‪ <singh20prabh@gmail.com>
2025-01-31 16:42:51 +0530

    navbar error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Story from "./components/Story";
 
 function App() {
